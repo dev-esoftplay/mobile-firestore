3eecba7c9bf89b17facf81661567cc17042f8a29
kholil <kholil@fisip.net>
2025-05-20 10:30:22 +0700

    update

--- master/publisher.js
+++ master/publisher.js
@@ -50,29 +50,20 @@ if (fs.existsSync("../mobile/config.json")) {
 }
 
 if (fs.existsSync("./package.json")) {
+	const shell = require('child_process').execSync;
 	const packJson = require("./package.json")
-	const letterVersion = "abcdefghijklmnopqrstuvwxyz"
+	const fs = require('fs');
 	const version = packJson.version
-	const letter = version.match(/([a-z])/g)
+	const vm = version.match(/([a-z])/g)
+	const letter = vm ? vm[0] : null
 	const number = version.replace(/-/g, "").replace(letter, "")
-	let nextLetter = ""
 	let nextNumber = number.split(".")[2]
 	let nextVersion = number.split(".")[0] + "." + number.split(".")[1] + "."
-	if (!letter) {
-		nextLetter = letterVersion[0]
-		nextVersion += nextNumber
-		nextVersion += "-" + nextLetter
-	} else if (letter != "z") {
-		nextLetter = letterVersion[letterVersion.indexOf(String(letter)) + 1]
-		nextVersion += nextNumber
-		nextVersion += "-" + nextLetter
-	} else {
-		nextNumber = Number(nextNumber) + 1
-		nextVersion += nextNumber
-	}
 
+	nextNumber = Number(nextNumber) + 1
+	nextVersion += nextNumber
 	const newPackJson = { ...packJson, version: nextVersion }
 	fs.writeFileSync("./package.json", JSON.stringify(newPackJson, undefined, 2))
-	shell("npm publish")
+	shell("npm publish", { stdio: ['inherit', 'inherit', 'inherit'] })
 	console.log("\nbun add esoftplay-" + moduleName + "@" + nextVersion + "\n")
 }
